e6a8db608fc388be81d4c7209d017c8ac92f29e4
Nandhakumarsj <sjnk0206123@outlook.com>
2024-02-26 11:39:37 +0530

    server update

--- iot/addHistory.py
+++ iot/addHistory.py
@@ -1,18 +1,21 @@
 import time
 import requests
 
-def addHistory(filename:str, url:str):
+
+def addHistory(filename: str, url: str):
     with open(filename, 'rb') as image:
         file = {
-                'image': image
+            'image': image
         }
         res = requests.post(
-        url, files=file
+            url, files=file
         )
         if res.status_code == 200:
             print('Uploaded')
         else:
             print('Not Uploaded')
-        
-file_name = time.strftime("%I-%M-%S-%p")
-addHistory(r'E:/Projects/IOT_Projects/secure-locker/assets/imgs/man1.jpg', f'http://127.0.0.1:8002/upload/{file_name}')
+
+
+file_name = time.strftime("%Y%m%d-%H%M%S")
+addHistory(r'E:/Projects/IOT_Projects/secure-locker/server/08-19-37-AM.jpg',
+           f'http://192.168.248.209:8080/upload/{file_name}')
